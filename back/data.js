2b0fedc11e726fc313d25bb51a4a45ac7a8d28e4
Ignacio Ureta <ignacio.ureta@live.com>
2018-02-04 22:25:25 -0300

    back ready

---
+++ back/data.js
@@ -0,0 +1,122 @@
+var movies = {
+  "success": true,
+  "station": "movies",
+  "items": [{
+          "id": 6,
+          "name": "Star Wars Ep. IV",
+          "excerpt": "Star Wars from George Lucas",
+          "price": 150,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/android-tv-6.jpg"
+      }, {
+          "id": 7,
+          "name": "Seven Samurai",
+          "excerpt": "Seven Samurai from Akira Kurosawa",
+          "price": 150,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/7samurai2.jpg"
+      },
+      {
+          "id": 8,
+          "name": "A Clockwork Orange",
+          "excerpt": "A Clockwork Orange from Stanley Kubrick",
+          "price": 150,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/clockwork.jpg"
+      }, {
+          "id": 9,
+          "name": "300",
+          "excerpt": "300 from Zack Snyder",
+          "price": 150,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/300.jpg"
+      },
+      {
+          "id": 10,
+          "name": "The Terminator",
+          "excerpt": "The Terminator from James Cameron",
+          "price": 150,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/the-terminator-1.jpg"
+      }
+  ]
+}
+
+var videogames = {
+  "success": true,
+  "station": "videogames",
+  "items": [{
+          "id": 11,
+          "name": "Super Mario Bros 64",
+          "excerpt": "Super Mario Bros 64",
+          "price": 80,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/mario64.jpg"
+      }, {
+          "id": 12,
+          "name": "The Legend Of Zelda: Ocarina Of Time",
+          "excerpt": "The Legend Of Zelda: Ocarina Of Time",
+          "price": 80,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/zelda.jpg"
+      },
+      {
+          "id": 13,
+          "name": "Megaman X",
+          "excerpt": "Megaman X",
+          "price": 80,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/megamanx.JPG"
+      }, {
+          "id": 14,
+          "name": "Borderlands",
+          "excerpt": "Borderlands",
+          "price": 80,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/borderlands.jpg"
+      },
+      {
+          "id": 15,
+          "name": "God Of War",
+          "excerpt": "God Of War",
+          "price": 80,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/godOfWar.jpg"
+      }
+  ]
+}
+
+var electronics = {
+  "success": true,
+  "station": "electronics",
+  "items": [{
+          "id": 1,
+          "name": "Android TV",
+          "excerpt": "Android TV Quad Core V.7.0",
+          "price": 150,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/android-tv-6.jpg"
+      }, {
+          "id": 2,
+          "name": "Speakers BT Genius",
+          "excerpt": "Speakers BT Genius 100 Watts",
+          "price": 200,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/geniusBT.JPG"
+      },
+      {
+          "id": 3,
+          "name": "Televisor AOC",
+          "excerpt": "Tv AOC 43 Inches, Smart TV",
+          "price": 300,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/AOC.jpg"
+      }, {
+          "id": 4,
+          "name": "Cafetera NESPRESSO",
+          "excerpt": "Cafetera NESPRESSO Inissia",
+          "price": 200,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/cafetera-nespresso.jpg"
+      },
+      {
+          "id": 5,
+          "name": "iPhone 6s 16 GB",
+          "excerpt": "iPhone 6s 16 GB Color Negro",
+          "price": 450,
+          "url": "https://snuuper.s3.amazonaws.com/snuuperTest/iphone.jpeg"
+      }
+  ]
+}
+
+module.exports = {
+  movies,
+  videogames,
+  electronics
+}
---
+++ back/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var data = require('./data')
+var app = express();
+
+app.get('/movies', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data.movies));
+});
+
+app.get('/videogames', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data.videogames));
+});
+
+app.get('/electronics', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data.electronics));
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
